c3d2dbdb023aa5eca75752557247ba9959fdbe6c
yelimpark <pyl9983@gmail.com>
2022-03-30 18:12:53 +0900

    Update project 8

--- Project8/Project8/main.c
+++ Project8/Project8/main.c
@@ -1,3 +1,6 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
 // C 라이브러리를 배워봅시다.
 // 단정문을 위한 라이브러리
 // 단정문 (assertion) : 조건을 검사할 때 사용하는 구문
@@ -8,6 +11,7 @@
 
 // 문자열 다루는 라이브러리
 #include <string.h>
+
 
 struct Player
 {
@@ -56,13 +60,89 @@ char* mystrstr(const char* str, const char* substr) {
 
 	int j = 0;
 	for (int i = 0; str[i] != 0; i++) {
-		if (substr[j] == 0) return &str[i-j];
-		
 		if (str[i] == substr[j]) { j++; }
 		else { j = 0; }
+
+		if (substr[j] == 0) return &str[i - j + 1];
 	}
 
 	return NULL;
+}
+
+int mystrcmp(const char* str1, const char* str2) {
+	int i;
+	for (i = 0; str1[i] != 0 && str2[i] != 0; i++) {
+		if (str1[i] < str2[i]) return -1;
+		else if (str1[i] > str2[i]) return 1;
+	}
+	if (str1[i] != 0) return 1;
+	if (str2[i] != 0) return -1;
+	return 0;
+};
+
+char* mystrtok(char* str, const char* deli) {
+	if (*deli == NULL) return str;
+
+	char* res;
+	static char* tmp;
+
+	if (str == NULL) res = tmp;
+	else res = str;
+
+	if (res == NULL) return NULL;
+
+	while (1) {
+		int isNotDeli = 1;
+		for (int j = 0; deli[j] != 0; j++) {
+			if (*res == deli[j]) {
+				res++;
+				isNotDeli = 0;
+			}
+		}
+		if (isNotDeli) break;
+	}
+
+	for (int i = 0; res[i] != 0; i++) {
+		for (int j = 0; deli[j] != 0; j++) {
+			if (res[i] == deli[j]) {
+				res[i] = '\0';
+				tmp = &res[i + 1];
+				return res;
+			}
+		}
+
+		if (res[i + 1] == 0) tmp = NULL;
+	}
+
+	return res;
+}
+
+char* mystrtok2(char* str, const char* deli) {
+	if (*deli == NULL) return str;
+
+	char* res;
+	static char* tmp;
+
+	if (str == NULL) res = tmp;
+	else res = str;
+
+	if (res == NULL) return NULL;
+
+	for (int i = 0; res[i] != 0; i++) {
+		for (int j = 0; deli[j] != 0; j++) {
+			if (res[i] == deli[j]) {
+				res[i] = '\0';
+				tmp = &res[i + 1];
+				return res;
+			}
+		}
+
+		if (res[i + 1] == 0) {
+			tmp = NULL;
+		}
+	}
+
+	return res;
 }
 
 int main() {
@@ -90,25 +170,80 @@ int main() {
 	//// 선언하면서 초기화하지 않은 문자열을 strcpy로 초기화해줬다
 	//strcpy(player2.name, "pokemon");
 
-	char str[] = "Poket Monster Sun And Moon Master";
-	size_t sz = mystrlen(str);
-	printf("길이 : %llu\n", sz);
-	printf("길이 : %llu\n", sz);
+	//const char* str = "Hello";
 
-	const char* res = strchr(str, 'M');
-	printf("찾은 위치 : %s\n", res);
-	const char* res2 = mystrchr(str, 'M');
-	printf("찾은 위치 : %s\n", res2);
+	//assert(5 == strlen(str));
+	//assert(0 == strlen(""));
 
-	const char* res3 = strrchr(str, 'M');
-	printf("찾은 위치 : %s\n", res3);
-	const char* res4 = mystrrchr(str, 'M');
-	printf("찾은 위치 : %s\n", res4);
+	//assert(str + 2 == strchr(str, 'l'));
+	//assert(NULL == strchr(str, 'a'));
 
-	const char* res5 = strstr(str, "Sun");
-	printf("찾은 위치 : %s\n", res5);
-	const char* res6 = mystrstr(str, "Sun");
-	printf("찾은 위치 : %s\n", res6);
+	//assert(str + 3 == strrchr(str, 'l'));
+	//assert(NULL == strrchr(str, 'a'));
 
+	//assert(str + 1 == strstr(str, "ell"));
+	//assert(str == strstr(str, ""));
+	//assert(NULL == strstr(str, "World"));
+
+	//assert(0 > strcmp(str, "Hello!"));
+	//assert(0 == strcmp(str, str));
+	//assert(0 < strcmp(str, "Hallo"));
+
+	//char str2[] = "A bird came down the walk";
+	//assert(0 == strcmp("A", strtok(str2, " ")));
+	//assert(0 == strcmp("bird", strtok(NULL, " ")));
+	//assert(0 == strcmp("came", strtok(NULL, " ")));
+	//assert(0 == strcmp("down", strtok(NULL, " ")));
+	//assert(0 == strcmp("the", strtok(NULL, " ")));
+	//assert(0 == strcmp("walk", strtok(NULL, " ")));
+	//assert(NULL == strtok(NULL, " "));
+
+	//char str3[] = "A,bird camezdown the walk";
+	//assert(0 == strcmp("A", strtok(str3, ", z")));
+	//assert(0 == strcmp("bird", strtok(NULL, ", z")));
+	//assert(0 == strcmp("came", strtok(NULL, ", z")));
+	//assert(0 == strcmp("down", strtok(NULL, ", z")));
+	//assert(0 == strcmp("the", strtok(NULL, ", z")));
+	//assert(0 == strcmp("walk", strtok(NULL, ", z")));
+	//assert(NULL == strtok(NULL, ", z"));
+
+	const char* str = "Hello";
+
+	assert(5 == mystrlen(str));
+	assert(0 == mystrlen(""));
+
+	assert(str + 2 == mystrchr(str, 'l'));
+	assert(NULL == mystrchr(str, 'a'));
+
+	assert(str + 3 == mystrrchr(str, 'l'));
+	assert(NULL == mystrrchr(str, 'a'));
+
+	assert(str + 1 == mystrstr(str, "ell"));
+	assert(str == mystrstr(str, ""));
+	assert(NULL == mystrstr(str, "World"));
+
+	assert(0 > mystrcmp(str, "Hello!"));
+	assert(0 == mystrcmp(str, str));
+	assert(0 < mystrcmp(str, "Hallo"));
+
+	char str2[] = "A bird came down the walk";
+	assert(0 == mystrcmp("A", mystrtok(str2, " ")));
+	assert(0 == mystrcmp("bird", mystrtok(NULL, " ")));
+	assert(0 == mystrcmp("came", mystrtok(NULL, " ")));
+	assert(0 == mystrcmp("down", mystrtok(NULL, " ")));
+	assert(0 == mystrcmp("the", mystrtok(NULL, " ")));
+	assert(0 == mystrcmp("walk", mystrtok(NULL, " ")));
+	assert(NULL == mystrtok(NULL, " "));
+
+	char str3[] = "A,,bird camezdown the walk";
+	assert(0 == mystrcmp("A", mystrtok(str3, ", z")));
+	assert(0 == mystrcmp("bird", mystrtok(NULL, ", z")));
+	assert(0 == mystrcmp("came", mystrtok(NULL, ", z")));
+	assert(0 == mystrcmp("down", mystrtok(NULL, ", z")));
+	assert(0 == mystrcmp("the", mystrtok(NULL, ", z")));
+	assert(0 == mystrcmp("walk", mystrtok(NULL, ", z")));
+	assert(NULL == mystrtok(NULL, ", z"));
+
+	return 0;
 
 }
